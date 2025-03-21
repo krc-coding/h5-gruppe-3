b32d7a59174d78257a22a8874b96d99207d8720d
Kasper K <Kasperklassen@gmail.com>
2025-03-21 10:33:20 +0100

    device controller

--- StoreDataCollection/app/Models/Devices.php
+++ StoreDataCollection/app/Models/Devices.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Devices extends Model
 {
+    public $timestamps = false;
     protected $fillable = [
         'uuid',
     ];
