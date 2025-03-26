66361482ad65f4de75789b423cf272538f315960
krc-coding <kasper@ulvedalen.dk>
2025-03-26 10:30:55 +0100

    Created factories for relevant models

--- StoreDataCollection/app/Models/Devices.php
+++ StoreDataCollection/app/Models/Devices.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Devices extends Model
 {
+    use HasFactory;
     public $timestamps = false;
     protected $fillable = [
         'uuid',
