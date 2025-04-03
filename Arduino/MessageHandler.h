d73e5f95072769cca7c7797baf90e22bfa3cf25a
krc-coding <kasper@ulvedalen.dk>
2025-04-03 11:59:52 +0200

    Updated message handler to work with new setup

--- Arduino/MessageHandler.h
+++ Arduino/MessageHandler.h
@@ -7,57 +7,41 @@ WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
 class MessageHandler {
-    private:
-    /* data */
-    const char broker[16] = "78.47.49.176";
-    int port = 1883;
+private:
+  Config config = Config();
+  int port = 1883;
 
-    public:
-    void init() {
-        char mac[18];
-        getMacAddress(mac);
-        String topic = String(mac) + "/response";
+public:
+  void init() {
+    char mac[18];
+    getMacAddress(mac);
+    String topic = config.topic;
 
-        Serial.print("Attempting to connect to the MQTT broker: ");
-        Serial.println(broker);
+    Serial.print("Attempting to connect to the MQTT broker: ");
+    Serial.println(config.mqtt_host);
 
-        if (!mqttClient.connect(broker, port)) {
-            Serial.print("MQTT connection failed! Error code = ");
-            Serial.println(mqttClient.connectError());
+    if (!mqttClient.connect(config.mqtt_host, port)) {
+      Serial.print("MQTT connection failed! Error code = ");
+      Serial.println(mqttClient.connectError());
 
-            while (1)
-                ;
-        }
-
-        Serial.println("You're connected to the MQTT broker!");
-        Serial.println();
-        
-        Serial.print("Subscribing to topic: ");
-        Serial.println(topic);
-        Serial.println();
-
-        // subscribe to a topic
-        mqttClient.subscribe(topic);
-
-        // topics can be unsubscribed using:
-        // mqttClient.unsubscribe(topic);
-
-        Serial.print("Topic: ");
-        Serial.println(topic);
-
-        Serial.println();
+      while (1)
+        ;
     }
 
-    void postMessage(float temperature) {
-        char mac[18];
+    Serial.println("You're connected to the MQTT broker!");
+    Serial.println();
+  }
 
-        getMacAddress(mac);
-        mqttClient.beginMessage(mac);
-        mqttClient.print("{\"temperature\":");
-        mqttClient.print(temperature);
-        mqttClient.print("}");
-        mqttClient.endMessage();
-    }
+  void postMessage(float temperature) {
+    char mac[18];
+
+    getMacAddress(mac);
+    mqttClient.beginMessage(mac);
+    mqttClient.print("{\"temperature\":");
+    mqttClient.print(temperature);
+    mqttClient.print("}");
+    mqttClient.endMessage();
+  }
 };
 
 #endif
